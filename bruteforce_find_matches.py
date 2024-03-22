69aed2b98c664b2c88e2e5ff070a77dc960cfd18
Bram Devlaminck <bram.devlaminck@gmail.com>
2024-03-22 16:57:09 +0100

    use multiprocessing

--- bruteforce_find_matches.py
+++ bruteforce_find_matches.py
@@ -1,4 +1,7 @@
 import json
+import multiprocessing
+from multiprocessing.shared_memory import ShareableList
+
 import regex as re
 from tqdm import tqdm
 
@@ -12,25 +15,56 @@ def read_tsv(file: str) -> list[tuple[str, str]]:
 
     return proteins
 
+def search_peptides(process_num: int, proteins: ShareableList, equalize_i_and_l: bool, peptides: list[str], return_dict):
+    for peptide in peptides:
+        peptide = peptide.rstrip("\n").upper()
+        search_peptide = peptide
+        if equalize_i_and_l:
+            search_peptide = re.sub("[IL]", "[IL]", search_peptide)
+
+        accessions = []
+        for accession_nr, sequence in proteins:
+            for _ in re.findall(search_peptide, sequence, overlapped=True):
+                accessions.append(accession_nr)
+
+        return_dict[process_num].append({
+            "sequence": peptide,
+            "uniprot_accessions": accessions
+        })
 
 def search_file(proteins: list[tuple[str, str]], peptide_file: str, output_file: str, equalize_i_and_l: bool = False):
+
     results = {"result": []}
+
+    num_cpu = multiprocessing.cpu_count()
+
+    peptides = []
     with open(peptide_file) as fp:
-        for peptide in tqdm(fp):
-            peptide = peptide.rstrip("\n").upper()
-            search_peptide = peptide
-            if equalize_i_and_l:
-                search_peptide = re.sub("[IL]", "[IL]", search_peptide)
+        for peptide in fp:
+            peptides.append(peptide)
 
-            accessions = []
-            for accession_nr, sequence in proteins:
-                for _ in re.findall(search_peptide, sequence, overlapped=True):
-                    accessions.append(accession_nr)
+    block_size = len(peptides) // num_cpu
 
-            results["result"].append({
-                "sequence": peptide,
-                "uniprot_accessions": accessions
-            })
+    manager = multiprocessing.Manager()
+    return_dict = manager.dict()
+    jobs = []
+    for i in range(num_cpu - 1):
+        return_dict[i] = manager.list()
+        p = multiprocessing.Process(target=search_peptides, args=(i, proteins, equalize_i_and_l, peptides[i*block_size:(i+1)*block_size], return_dict))
+        jobs.append(p)
+        p.start()
+
+    # handle last iteration separately to ensure that we don't skip some data at the end
+    return_dict[num_cpu - 1] = manager.list()
+    p = multiprocessing.Process(target=search_peptides, args=(num_cpu - 1, proteins, equalize_i_and_l, peptides[(num_cpu - 1) * block_size:], return_dict))
+    jobs.append(p)
+    p.start()
+
+    for proc in jobs:
+        proc.join()
+
+    # flatten output from subprocesses and save result
+    results["result"] = [j for sub in return_dict.values() for j in sub]
 
     with open(output_file, "w") as output_fp:
         json.dump(results, output_fp)
